56009909ac525366fbd4839600d87b1770462fc1
Luis Ramsés Monjarrez Solis <lmonjarrez91@gmail.com>
2026-08-09 19:28:19 -0600

    feat: integrate Trainer into main menu

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from src.models.model import EduGPT
 from src.datasets.corpus_builder import CorpusBuilder
 from src.tokenizer.tokenizer import Tokenizer
+from src.training.trainer import Trainer
 
 def main():
     model = EduGPT()
     builder = CorpusBuilder()
     tokenizer = Tokenizer()
+    trainer = Trainer(builder)
 
     while True:
         option = model.show_menu()
         if option == "1":
             builder.build_corpus()
         elif option == "2":
-            model.train()
+            model.train(trainer)
         elif option == "3":
             model.show_information()
         elif option == "4":
@@ -29,6 +31,7 @@ def main():
             break
         else:
             print("Opción no válida.")
+
 
 if __name__ == "__main__":
     main()
